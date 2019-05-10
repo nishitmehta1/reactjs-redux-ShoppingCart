fca6af087ef50dda05f5caaa1639e703228d4683
Nishit Mehta <nishit.mehta1@gmail.com>
2019-05-10 14:28:31 -0500

    Updates

---
+++ precache-manifest.f226026e0b165eaec3b054d65b66bf8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d202842bf4dea750a3d50c553fba14d",
+    "url": "/reactjs-redux-ShoppingCart/index.html"
+  },
+  {
+    "revision": "a8e15a4b504dd8aed38e",
+    "url": "/reactjs-redux-ShoppingCart/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d41bca1ace5048bbddb3",
+    "url": "/reactjs-redux-ShoppingCart/static/css/main.25fae144.chunk.css"
+  },
+  {
+    "revision": "a8e15a4b504dd8aed38e",
+    "url": "/reactjs-redux-ShoppingCart/static/js/2.6644bb4d.chunk.js"
+  },
+  {
+    "revision": "d41bca1ace5048bbddb3",
+    "url": "/reactjs-redux-ShoppingCart/static/js/main.0b50a5ec.chunk.js"
+  },
+  {
+    "revision": "292af8a8149351013913",
+    "url": "/reactjs-redux-ShoppingCart/static/js/runtime~main.63aac590.js"
+  }
+]);
